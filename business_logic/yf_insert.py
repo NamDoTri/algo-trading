25f465c06271eb6e3d20c39456c8f42e95210ae2
NamDoTri <trinam9999@gmail.com>
2021-03-24 22:46:13 +0200

    Added yf daily price insert test

--- business_logic/yf_insert.py
+++ business_logic/yf_insert.py
@@ -4,11 +4,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import yfinance as yf
 from MySQLdb.cursors import Cursor
 from pandas import DataFrame, MultiIndex
+from pandas.core.indexes.datetimes import DatetimeIndex
 from database.data_manager.data_access import connect_as_user
 from database.data_manager.init_queries import insert_daily_price_query
 from business_logic.validation import security_exists
 from business_logic.insert import insert_city, insert_currency, insert_exchange, insert_security
-from business_logic.get_data import fetch_info_of, get_currency_ids, get_exchange_ids, get_security_ids, get_security_symbols
+from business_logic.get_data import get_currency_ids, get_exchange_ids, get_security_ids, get_security_symbols
 
 def insert_yf_ticker(new_ticker):
     """
@@ -36,10 +37,11 @@ def insert_yf_ticker(new_ticker):
     else:
         raise Exception('Method requires object of class yfinance.Ticker.')
 
-def bulk_insert_yf_daily_price(lst_symbols, dataset, *, data_vendorID, cursor = None):
+def bulk_insert_yf_daily_price(lst_symbols, dataset, *, data_vendorID, cursor = None) -> int:
     """
         data: pd.DataFrame of records of more than 1 symbol
         lst_symbols: list of symbols contained in the DataFrame as strings
+        Returns number of rows affected
     """
     if len(lst_symbols) == 0:
         raise Exception('A list of symbols contained in the dataset must be specified')
@@ -49,6 +51,8 @@ def bulk_insert_yf_daily_price(lst_symbols, dataset, *, data_vendorID, cursor = 
         if isinstance(dataset.columns, MultiIndex):
             csr = cursor if cursor is Cursor else connect_as_user().cursor()
             lst_db_symbols = get_security_symbols(cursor=csr)
+            number_of_rows = 0
+
             for symbol in lst_symbols:
                 if not (symbol in lst_db_symbols):
                     ticker = yf.Ticker(symbol)
@@ -56,22 +60,46 @@ def bulk_insert_yf_daily_price(lst_symbols, dataset, *, data_vendorID, cursor = 
                     
                 securityID = get_security_ids(f"WHERE abbrev = '{symbol}'")
                 data = dataset[symbol]
-                insert_yf_daily_price(securityID, data, data_vendorID=data_vendorID)
+                data = parse_price_df(data)
+                number_of_rows += insert_yf_daily_price(securityID, data, data_vendorID=data_vendorID)
+
+            return number_of_rows
+        else:
+            raise Exception('Unable to parse data from DataFrame')
 
 
-def insert_yf_daily_price(securityID, data, *, data_vendorID, cursor = None):
+def insert_yf_daily_price(securityID, data, *, data_vendorID = 0, cursor = None) -> int:
     """
         data: pd.DataFrame of only 1 symbol
         data_vendor is default to be Yahoo Finance
     """
-    if isinstance(data, DataFrame):
-        csr = cursor if cursor is Cursor else connect_as_user().cursor()
-        lst_data = list(data.to_records())
-        query = insert_daily_price_query(securityID, data_vendorID, '%s', '%s', '%s', '%s', '%s', '%s', '%s')
-        csr.executemany(query, lst_data)
+    if isinstance(data, list):
+        if len(data) > 0:
+            if len(data[0]) == 6 :
+                csr = cursor if cursor is Cursor else connect_as_user().cursor()
+                query = insert_daily_price_query(securityID, data_vendorID, '%s', '%s', '%s', '%s', '%s', '%s')
+                number_of_rows = csr.executemany(query, data)
+                return number_of_rows
+            else: 
+                raise Exception('Number of columns is not correct.')
+        else:
+            raise Exception('Cannot insert an empty dataset.')
     else:
-        raise Exception('Dataset is of unsupported type')
+        raise Exception('Dataset is of unsupported type.')
 
-
+def parse_price_df(orig_data) -> list:
+    """
+        Parse DataFrame of security price from yfinance
+    """
+    if isinstance(orig_data, DataFrame):
+        if (orig_data.shape[1] >= 5) and isinstance(orig_data.index, DatetimeIndex):
+            data = orig_data.copy()
+            data = data.iloc[:, 0:5] # slice necessary columns
+            data.index = data.index.strftime('%d-%m-%Y')
+            return data.to_records(index=True).tolist()
+        else:
+            raise Exception("DataFrame is not in a correct shape")
+    else:
+        raise Exception("Input is not a DataFrame")
 
 
--- tests/yf_insert_test.py
+++ tests/yf_insert_test.py
@@ -1,32 +1,36 @@
-from multiprocessing import Value
 import unittest
 import yfinance as yf
-from MySQLdb.cursors import Cursor
 
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from business_logic.yf_insert import insert_yf_ticker, insert_yf_daily_price, bulk_insert_yf_daily_price
-from business_logic.get_data import get_exchange_ids, get_security_ids, get_currency_ids
+from business_logic.yf_insert import insert_yf_ticker, insert_yf_daily_price, bulk_insert_yf_daily_price, parse_price_df
+from business_logic.get_data import get_exchange_ids, get_security_ids, get_currency_ids, get_security_symbols
 from database.data_manager.data_access import connect_as_user
 from db_tests import DbInitializationTest
 from helpers.printing import mute_log, unmute_log
 
 class yFinanceInsertTest(unittest.TestCase):
     def setUp(self):
+        print('setUp method called.')
         self._cursor = connect_as_user().cursor()
         mute_log()
         db_reset = DbInitializationTest()
         db_reset.run_tests()
         unmute_log()
 
+        self._ticker = yf.Ticker('AAPL')
+
+        if self._testMethodName == self.test_insert_daily_price.__name__:
+            insert_yf_ticker(self._ticker)
+
     @property
     def cursor(self):
         return self._cursor
 
     def test_insert_yf_ticker_with_valid_ticker(self):
-        symbol = 'AAPL'
-        ticker = yf.Ticker(symbol)
+        ticker = self._ticker
+        symbol = self._ticker
         exchange = ticker.info['exchange']
         currency = ticker.info['currency']
         insert_yf_ticker(ticker)
@@ -47,7 +51,11 @@ class yFinanceInsertTest(unittest.TestCase):
         self.assertRaises(ValueError, insert_yf_ticker, ticker)
 
     def test_insert_daily_price(self):
-        pass
+        securityID = get_security_ids("WHERE abbrev = 'AAPL'")[0]
+        data = self._ticker.history(period='1mo')
+        data = parse_price_df(data) # -> a list
+        num_affected_rows = insert_yf_daily_price(securityID, data)
+        self.assertEqual(num_affected_rows, len(data))
 
     def test_bulk_insert_daily_price(self):
         pass
