c8499dfb9e06a88e3fa1d3e72e131165a9893fe2
NamDoTri <trinam9999@gmail.com>
2021-04-06 10:27:58 +0300

    Added methods for saving and loading models and their tests

---
+++ business_logic/model_crud.py
@@ -0,0 +1,47 @@
+from typing import Collection
+import pymongo
+import pickle
+import time
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), '..')))
+from database.mongo_client import get_mongo_db_conn
+
+def save_model_to_mongo(model, model_name, db_connection = None) -> str:
+    '''
+        Pickle object model and save to MongoDB if not exists.
+        Return: Id of the newly saved model
+    '''
+    conn = db_connection if db_connection is pymongo.database.Database else get_mongo_db_conn()
+
+    if not model_exists(model_name, db_connection=conn):
+        pickled = pickle.dumps(model)
+        new_obj = conn.insert_one({'model': pickled, 'model_name': model_name, 'created_at': time.time(), 'last_trained': time.time()})
+        return new_obj.inserted_id
+    else:
+        raise Exception(f"A model with name {model_name} already exists.")
+
+
+def load_saved_model_from_mongo(model_name, db_connection = None) -> object:
+    '''
+        Query model from MongoDB if exists, then parse it.
+        Return: binary value from the query parsed into a Python object
+    '''
+    conn = db_connection if db_connection is pymongo.database.Database else get_mongo_db_conn()
+    res = conn.find_one({'model_name': model_name})
+    if res is None:
+        raise Exception(f'No model with the name {model_name} can be found in the database.')
+    else:
+        model = pickle.loads(res['model'])
+        return model
+
+
+def model_exists(model_name, db_connection = None) -> bool:
+    if not isinstance(model_name, str):
+        raise TypeError('Model name must be a string.')
+    
+    conn = db_connection if isinstance(db_connection, pymongo.collection.Collection) else get_mongo_db_conn()
+    res = conn.find_one({'model_name': model_name})
+    return not res is None
+
+    
--- tests/mongodb_tests.py
+++ tests/mongodb_tests.py
@@ -3,13 +3,35 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from database.mongo_client import get_mongo_db_conn
 from pymongo.collection import Collection
+from database.mongo_client import get_mongo_db_conn, reset_db
+from business_logic.model_crud import save_model_to_mongo, load_saved_model_from_mongo
 
 class MongoDBConnectionTest(unittest.TestCase):
-    def test_connection(self):
+    MODEL_NAME = 'TestModel'
+
+    def setUp(self):
+        reset_db()
+
+    def test1_connection(self):
         db_conn = get_mongo_db_conn()
         self.assertIsInstance(db_conn, Collection)
+
+    def test2_saving_model(self):
+        conn = get_mongo_db_conn()
+        new_ID = save_model_to_mongo('Test string as object', self.MODEL_NAME, conn)
+        res = conn.find_one({'model_name': self.MODEL_NAME})
+        saved_ID = res['_id']
+
+        self.assertEqual(new_ID, saved_ID)
+
+    def test3_loading_model(self):
+        conn = get_mongo_db_conn()
+        save_model_to_mongo('Test string as object', self.MODEL_NAME, conn)
+        res = load_saved_model_from_mongo(self.MODEL_NAME)
+        self.assertIsInstance(res, str)
+
+
 
 if __name__ == '__main__':
     unittest.main()
