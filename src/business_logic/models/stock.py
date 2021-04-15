2cda949f62113cb8f569b14d16b0e7d116903418
NamDoTri <trinam9999@gmail.com>
2021-04-15 12:36:31 +0300

    Added method for calculating number of shares to buy

--- src/business_logic/models/stock.py
+++ src/business_logic/models/stock.py
@@ -1,4 +1,6 @@
+from numpy.lib.arraysetops import isin
 from yfinance import Ticker
+import math
 from business_logic.validation import ticker_is_valid
 
 class Stock:
@@ -22,6 +24,10 @@ class Stock:
 
     def total_worth(self):
         return self.buy_price * self.num_shares
+
+    def set_stock_quantity(self, price_per_share, cash):
+        if isinstance(price_per_share, float) and isinstance(cash, float):
+            self.num_shares = math.floor(cash/price_per_share)
 
     def __str__(self) -> str:
         return ', '.join([self.symbol, self.buy_price, self.num_shares])
